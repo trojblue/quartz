9a1633778b4de9c6bfe3c91306be7b5e1a67c1c3
Xinlei Xu <38530926+Trojblue@users.noreply.github.com>
2022-03-06 05:56:04 -0500

    automatic commit 2022/03/06 :  5:56:04.17

--- update.bat
+++ update.bat
@@ -1,7 +1,11 @@
 @echo off
-cd D:\CSC2\Obsidian-wiki-fix\src
+
+set FIX_DIR = "D:\CSC2\Obsidian-wiki-fix\src"
+set QUARTZ_DIR = "D:\quartz"
+
+cd %SOURCE_DIR%
 python app.py
-cd D:\quartz
+cd %QUARTZ_DIR%
 
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
@@ -11,6 +15,6 @@ git commit -m "automatic commit %mydate% : %mytime%"
 git push
 
 
-echo "自动上传完成! 等待10秒退出"
+echo "upload done! will auto-exit after 10 seconds"
 choice /d y /t 10 > nul
-echo "溜了溜了.."
+echo "exit.."
